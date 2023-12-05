55dea0cd2b0d59dc8e198e2b5883fb0f6109da39
MirkoCzo <Mirko.cuozzo@hotmail.com>
2023-12-05 16:30:22 +0100

    First

---
+++ src/app/components/formateur/formateur.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formateur',
+  templateUrl: './formateur.component.html',
+  styleUrl: './formateur.component.css'
+})
+export class FormateurComponent {
+
+}
---
+++ src/app/components/session-cours/session-cours.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-session-cours',
+  templateUrl: './session-cours.component.html',
+  styleUrl: './session-cours.component.css'
+})
+export class SessionCoursComponent {
+
+}
