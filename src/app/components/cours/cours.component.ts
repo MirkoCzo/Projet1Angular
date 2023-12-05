496331d72df97c4d31b9416f07ca66b84f2d68da
MirkoCzo <Mirko.Cuozzo@hotmail.com>
2023-12-05 18:00:50 +0100

    add cours component

--- src/app/components/cours/cours.component.ts
+++ src/app/components/cours/cours.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Cours} from "../../entities/cours.entities";
+import {CoursService} from "../../services/cours.service";
+import {Observable} from "rxjs";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-cours',
@@ -6,5 +10,45 @@ import { Component } from '@angular/core';
   styleUrl: './cours.component.css'
 })
 export class CoursComponent {
+  cours?: Cours[];
+
+  constructor(private coursService: CoursService, private router:Router) {
+  }
+
+  ngOnInit(): void{
+
+  }
+
+  onSearch(value: any){
+    this.coursService.findCoursByMatiere(value.matiere).subscribe(
+      data=>{
+        this.cours = data
+      }
+    )
+  }
+
+  onNewCours(){
+    this.router.navigateByUrl('newCours');
+  }
+
+  onDelete(c: Cours) {
+    let v = confirm("Êtes-vous sûr de vouloir supprimer?");
+    if (v) {
+      this.coursService.deleteCours(c).subscribe(
+        () => { // Fonction de succès
+          this.onSearch(c);
+        },
+        err => { // Fonction d'erreur
+          alert(err.headers.get("error"));
+        }
+      );
+    }
+  }
+
+  onEdit(c: Cours)
+  {
+    this.router.navigateByUrl('editCours/'+c.idcours);
+  }
+
 
 }
